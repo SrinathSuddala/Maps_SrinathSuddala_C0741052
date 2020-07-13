4ad7dbf60078ac87e2b2f646041053d4a4d5b4f4
Srinath <srinathsuddala786@gmail.com>
2020-07-13 12:44:24 +0530

    Added marker at current location

--- app/src/test/java/com/example/maps_srinathsuddala_c0741052/ExampleUnitTest.java
+++ app/src/test/java/com/example/maps_srinathsuddala_c0741052/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.example.maps_srinathsuddala_c0741052;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
